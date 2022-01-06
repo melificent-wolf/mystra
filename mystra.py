670f03280404330acbc63883fa8ecc4a227ca526
Melissa <github@kyndimarion.net>
2022-01-05 23:52:10 -0500

    Add logging and welcomer.  DM owner on user join.

--- mystra.py
+++ mystra.py
@@ -1,11 +1,32 @@
 #!/usr/bin/python
 
 import os
+import os.path
 import sys
+
+import logging
+from logging.handlers import RotatingFileHandler
 
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+
+# Set up logging
+
+log_dir = "logs"
+log_filename = f"{log_dir}/mystra.log"
+
+if not os.path.exists(log_dir):
+    os.mkdir(log_dir, mode=0755)
+
+max_log_size = 10 * 1024 * 1024
+handler = RotatingFileHandler(log_filename, maxBytes=max_log_size, backupCount=4)
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(handler)
+
+
+# Load config
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -21,6 +42,7 @@ description = "The stars are out and magic is here"
 
 bot = commands.Bot(command_prefix=";", description=description, intents=intents)
 
+guild = None
 
 @bot.event
 async def on_ready():
@@ -29,11 +51,13 @@ async def on_ready():
 
 
 @bot.event
-async def on_guild_available(guild):
-    print(f"Connected to {guild.name}")
+async def on_guild_available(g):
+    global guild
+    guild = g
+    print(f"Connected to {g.name}")
     print("Members:")
 
-    for member in [m for m in guild.members if not m.bot]:
+    for member in [m for m in g.members if not m.bot]:
         if member.nick:
             print(f"  {member.nick} ({member.name}#{member.discriminator})")
         else:
@@ -41,6 +65,19 @@ async def on_guild_available(guild):
 
     print("------")
     
+
+@bot.event
+async def on_member_join(member):
+    msg = f"{member.mention} has arrived.  Welcome to the Mystic Inn!  Come sit for a spell!"
+    await guild.system_channel.send(msg)
+    await guild.owner.send(f"{member.name} has joined")
+
+
+@bot.event
+async def on_member_remove(member):
+    msg = f"Bye, {member.display_name}, we hope you enjoyed your stay here."
+    await guild.system_channel.send(msg)
+
 
 @bot.command()
 @commands.has_role(MOD_ROLE)
